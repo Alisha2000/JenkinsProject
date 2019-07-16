2923f549dc767288f393835cc5e25c58f9da211f
Asiyat <amely1988@bk.ru>
2019-07-15 21:38:43 -0500

    commiting

---
+++ src/test/java/loops.java
@@ -0,0 +1,10 @@
+public class loops {
+    public static void main(String[] args) {
+
+
+        String word = "Asiyat";
+        for (int i = 0; i < word.length();i++){
+            System.out.println("I'm perfect in Jenkins");
+        }
+    }
+}
